f80d736ac8d58e064529cb87539483c39155ae06
shiziksama <karepinandrei@gmail.com>
2018-12-20 17:34:23 +0300

    some fixes to menu

--- src/js/main.js
+++ src/js/main.js
@@ -15,21 +15,21 @@ $(document).ready(function(){
 // sub-menu 
     
     $('.menu li.menu-item-has-children>a').click(function(e){
-		e.preventDefault();
 		if(window.innerWidth>1279)return ;
+		e.preventDefault();
 		$('.sub-menu').not($(this).closest('li').find('.sub-menu')).slideUp('300');
 		$(this).closest('li').find('.sub-menu').slideToggle('300');
     }); 
     
     $('.menu li.sub-menu-item-has-children>a').click(function(e){
-        e.preventDefault();
 		if(window.innerWidth>1279)return ;
+        e.preventDefault();
         $('.sub-menu ul').not($(this).closest('li').find(' ul')).slideUp('300');	$(this).closest('li').find('ul:first').slideToggle('300');
     }); 
     
     $('.menu li.small-sub-menu-item-has-children>a').click(function(e){
-        e.preventDefault();
 		if(window.innerWidth>1279)return ;
+        e.preventDefault();
         $('.sub-menu ul').not($(this).closest('li').find(' ul')).not($(this).closest('li').closest('ul')).slideUp('300');	$(this).closest('li').find('ul:first').slideToggle('300');
     });
 }); 
